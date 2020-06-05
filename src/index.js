c98f7fff3b8bc3c188470052b6d26d717b0e7a3a
Iain Lane <iain@orangesquash.org.uk>
2020-06-05 21:25:19 +0100

    Shuffle the string

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import Two from 'two.js';
 
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
 function setCircleSize(circle) {
     circle.width = two.width / 2
     circle.height = two.height / 2
@@ -7,7 +20,7 @@ function setCircleSize(circle) {
     circle.translation.set(circle.width, circle.height);
 }
 
-var text = 'this is a test anagram';
+var text = 'this is a test anagram'.replace(/\s/g, '').shuffle();
 
 var two = new Two({fullscreen: true}).appendTo(document.body);
 
