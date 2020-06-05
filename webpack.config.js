b0dc9d4510f851e230c0a3097919f67eb31fbd2a
Iain Lane <iain@orangesquash.org.uk>
2020-06-05 21:08:18 +0100

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  devtool: 'inline-source-map',
   entry: {
     app: './src/index.js',
   },
- plugins: [
-   new HtmlWebpackPlugin({
-     title: 'anagram.ninja'
-   }),
- ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'anagram.ninja'
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
